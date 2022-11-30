bb8511f007da06c318df93dddf67d4186e9f38b6
Kingzbrian <murageb254@gmail.com>
2022-11-30 05:01:45 -0800

    "fiz buzz"

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -4,7 +4,7 @@ def fizzbuzz():
         if i % 15 == 0:
             print("FizBuzz", end=" ")
         elif i % 3 == 0:
-            print("Fiz", end=" ")
+            print("Fizz", end=" ")
         elif i % 5 == 0:
             print("Buzz", end=" ")
         else:
