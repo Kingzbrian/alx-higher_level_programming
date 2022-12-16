3b3361d7de5871383e6b5b4eded1ce37d1325808
Kingzbrian <murageb254@gmail.com>
2022-12-16 02:08:15 -0800

    "kkk"

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,45 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
-    from calculator_1 import *
-    count = len(argv)
-
-    if count != 4:
-        print("Usage: {} <a> <operator> <b>".format(argv[0]))
-        exit(1)
-
-    num1 = int(argv[1])
-    num2 = int(argv[3])
-    op = argv[2]
-
-    def not_found():
-        print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
-
-    def add_():
-        total = add(num1, num2)
-        print("{:d} + {:d} = {:d}".format(num1, num2, total))
-        return total
-
-    def sub_():
-        total = sub(num1, num2)
-        print("{:d} - {:d} = {:d}".format(num1, num2, total))
-        return total
-
-    def mul_():
-        total = mul(num1, num2)
-        print("{:d} * {:d} = {:d}".format(num1, num2, total))
-        return total
-
-    def div_():
-        total = div(num1, num2)
-        print("{:d} / {:d} = {:d}".format(num1, num2, total))
-        return total
-
-    options = {
-        "+": add_,
-        "-": sub_,
-        "*": mul_,
-        "/": div_
-    }
-    options.get(op, not_found)()
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
