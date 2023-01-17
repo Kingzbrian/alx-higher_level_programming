7de5c6f0cac477bbaf342823be60be8ae2156dcb
Kingzbrian <murageb254@gmail.com>
2023-01-17 00:07:19 -0800

    "kk"

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -13,7 +13,6 @@ def inherits_from(obj, a_class):
         True if obj isinstance of a_class/ class that inherits from it.
         otherwise False
     """
-
-    if issubclass(type(obj), a_class) and type(obj) != a_class:
+    if isinstance(obj, a_class) and obj.__class__ is not a_class:
         return True
     return False
