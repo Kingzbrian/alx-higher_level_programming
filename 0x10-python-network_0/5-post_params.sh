f84ed8e97bec00b6b2ad169714ea0577c5ff0d08
Kingzbrian <murageb254@gmail.com>
2023-04-26 01:48:02 -0700

    "king"

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # takes in URL, sends POST request to passed URL, displays body of the response
-curl -sd "email=test@gmail.com&subject=I will always be here for PLD" "$1" -X POST
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
 
