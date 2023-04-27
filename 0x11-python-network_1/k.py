e83bb4d6b32c3e3d2ccea7667862782fe68eb85b
Kingzbrian <murageb254@gmail.com>
2023-04-27 00:46:48 -0700

    "add"

--- 0x11-python-network_1/k.py
+++ 0x11-python-network_1/k.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""fetches https://intranet.hbtn.io/status"""
+#fetching a urlusing urllib
 import urllib.request
 with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as responce:
     html = responce.read
-
