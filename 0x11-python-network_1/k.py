26abc609e63a63efb6bd21d551759869deeaf7c0
Kingzbrian <murageb254@gmail.com>
2023-04-27 01:49:05 -0700

    "add"

--- 0x11-python-network_1/k.py
+++ 0x11-python-network_1/k.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
-#fetching a urlusing urllib
-import urllib.request
-with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as responce:
-    html = responce.read
+"""Take in a URL, send request to URL and display value of `X-Request-Id`"""
+import sys
+from urllib import request
+
+if __name__ == "__main__":
+    value = {"email": sys.argv[2]}
+    data = parse.urlencode(value)
+    data = data.encode('utf-8')
+    req = request.Request(sys.argv[1],  data)
+    with request.urlopen(req) as res:
+        print(res.read.decode("utf-8"))
+
