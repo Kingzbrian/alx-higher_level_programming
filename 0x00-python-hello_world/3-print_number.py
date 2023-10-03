52360c2efe930aa02103d49a671f284505bdec8a
Kingzbrian <murageb254@gmail.com>
2023-10-03 10:06:08 +0300

    "elsr"

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 number = 98
-print(f"{number} Battery street")
+if isinstance(number, str):
+    raise ValueError("Unknown format code 'd' for object of type 'str'")
+else:
+    print(f"{number} Battery street")
