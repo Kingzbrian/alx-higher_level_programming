dcb02e3237c69779cc9dcb8c1a16d3521d826622
Kingzbrian <murageb254@gmail.com>
2023-10-05 14:36:40 +0300

    "addition"

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,9 +1,24 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
-    a = 10
-    b = 5
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+    from sys import argv, exit
+    j = len(argv)
+    if j != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    a = int(argv[1])
+    b = int(argv[3])
+    op = argv[2]
+    operators = {
+            '+': add,
+            '-': sub,
+            '*': mul,
+            '/': div
+            }
+    if op not in operators:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+    result = operators[op](a, b)
+    print("{} {} {} = {}".format(a, op, b, result))
+    exit(0)
