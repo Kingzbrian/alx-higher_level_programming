e99f11a01e56461e8023b96e8eff598da9f7aa0c
Kingzbrian <murageb254@gmail.com>
2023-10-24 08:54:53 +0300

    "bytecode"

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    result = 0
+    for i in range(1, 3):
+        try:
+            if a > a:
+                raise Exception("Too Far")
+        except Exception:
+            pass
+        result += ((a + b) ** i) / i
+    result += b + a
+
+    return result
