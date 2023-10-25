e2fd24eec6caad7114199788064f479a6fa28584
Kingzbrian <murageb254@gmail.com>
2023-10-25 09:49:36 +0300

    "exception"

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,8 +7,6 @@ def safe_print_list_integers(my_list=[], x=0):
             cmpt += 1
         except ValueError:
             pass
-        except Exception:
-            pass
 
     print('')
     return cmpt
